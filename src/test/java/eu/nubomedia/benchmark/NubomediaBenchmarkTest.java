704e27672d893f54712c92c9e2952068f822ff3a
Boni Garcia <bgarcia@gsyc.es>
2016-06-30 13:33:45 +0200

    Enable latency/quality measurements in test

--- src/test/java/eu/nubomedia/benchmark/NubomediaBenchmarkTest.java
+++ src/test/java/eu/nubomedia/benchmark/NubomediaBenchmarkTest.java
@@ -15,6 +15,7 @@
 
 package eu.nubomedia.benchmark;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
@@ -25,7 +26,6 @@ import org.kurento.test.base.BrowserTest;
 import org.kurento.test.browser.Browser;
 import org.kurento.test.browser.BrowserType;
 import org.kurento.test.browser.WebPage;
-import org.kurento.test.browser.WebPageType;
 import org.kurento.test.config.BrowserConfig;
 import org.kurento.test.config.BrowserScope;
 import org.kurento.test.config.TestScenario;
@@ -44,38 +44,38 @@ public class NubomediaBenchmarkTest extends BrowserTest<WebPage> {
   @Parameters(name = "{index}: {0}")
   public static Collection<Object[]> data() {
     TestScenario test = new TestScenario();
-    test.addBrowser(BrowserConfig.PRESENTER,
-        new Browser.Builder().webPageType(WebPageType.ROOT).browserType(BrowserType.CHROME)
-            .scope(BrowserScope.LOCAL).url("https://localhost:8443/").build());
-    test.addBrowser(BrowserConfig.VIEWER,
-        new Browser.Builder().webPageType(WebPageType.ROOT).browserType(BrowserType.CHROME)
-            .scope(BrowserScope.LOCAL).url("https://localhost:8443/").build());
+    test.addBrowser(BrowserConfig.PRESENTER, new Browser.Builder().browserType(BrowserType.CHROME)
+        .scope(BrowserScope.LOCAL).url("https://localhost:8443/").build());
+    test.addBrowser(BrowserConfig.VIEWER, new Browser.Builder().browserType(BrowserType.CHROME)
+        .scope(BrowserScope.LOCAL).url("https://localhost:8443/").build());
     return Arrays.asList(new Object[][] { { test } });
   }
 
   @Test
   public void testNubomedia() throws Exception {
-    // Start presenter
-    getPresenter().getBrowser().getWebDriver().findElement(By.id("presenter")).click();
-    // TODO enable
-    // getPresenter().subscribeEvent("video", "playing");
-    // getPresenter().waitForEvent("playing");
-
-    // Start viewer
-    getViewer().getBrowser().getWebDriver().findElement(By.id("viewer")).click();
-    // TODO enable
-    // getViewer().subscribeEvent("video", "playing");
-    // getViewer().waitForEvent("playing");
-
     // Sync presenter and viewer time
     WebPage[] browsers = { getPresenter(), getViewer() };
     String[] videoTags = { "video", "video" };
     String[] peerConnections = { "webRtcPeer.peerConnection", "webRtcPeer.peerConnection" };
     syncTimeForOcr(browsers, videoTags, peerConnections);
 
+    // Start presenter
+    getPresenter().getBrowser().getWebDriver().findElement(By.id("presenter")).click();
+    getPresenter().subscribeEvent("video", "playing");
+    getPresenter().waitForEvent("playing");
+
+    // Start viewer
+    getViewer().getBrowser().getWebDriver().findElement(By.id("viewer")).click();
+    getViewer().subscribeEvent("video", "playing");
+    getViewer().waitForEvent("playing");
+
     // Start OCR
     getPresenter().startOcr();
     getViewer().startOcr();
+
+    // Start recordings
+    getPresenter().startRecording("webRtcPeer.peerConnection.getLocalStreams()[0]");
+    getViewer().startRecording("webRtcPeer.peerConnection.getRemoteStreams()[0]");
 
     // Play video
     waitSeconds(PLAYTIME_SEC);
@@ -84,12 +84,22 @@ public class NubomediaBenchmarkTest extends BrowserTest<WebPage> {
     Map<String, Map<String, String>> presenterMap = getPresenter().getOcrMap();
     Map<String, Map<String, String>> viewerMap = getViewer().getOcrMap();
 
+    // Stop recordings
+    getPresenter().stopRecording();
+    getViewer().stopRecording();
+
+    // Store recordings
+    File presenterFileRec = getPresenter().getRecording("presenter.webm");
+    File viewerFileRec = getViewer().getRecording("viewer.webm");
+
     // Finish OCR, close browser, release media pipeline
     getPresenter().endOcr();
     getViewer().endOcr();
-    // TODO enable
-    // getPresenter().close();
-    // getViewer().close();
+    getPresenter().close();
+    getViewer().close();
+
+    // Process data and write quality metrics
+    getQuality(presenterFileRec, viewerFileRec, this.getClass().getSimpleName() + "-qov.csv");
 
     // Process data and write latency/statistics
     processDataToCsv(this.getClass().getSimpleName() + ".csv", presenterMap, viewerMap);
